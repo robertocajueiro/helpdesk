5ec07f97feeffd207aec7cdea4ba88ad94eccccf
robertocajueiro <beto.cajueiro@gmail.com>
2023-01-15 20:24:59 -0300

    endpoint para findById Tecnico

---
+++ src/main/java/com/roberto/helpdesk/services/TecnicoService.java
@@ -0,0 +1,22 @@
+package com.roberto.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.roberto.helpdesk.domain.Tecnico;
+import com.roberto.helpdesk.repository.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+	
+	@Autowired
+	private TecnicoRepository repository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+
+}
