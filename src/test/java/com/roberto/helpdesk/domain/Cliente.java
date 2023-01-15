8aa662d485166f05372fddea3a0fb8003722e29e
robertocajueiro <beto.cajueiro@gmail.com>
2023-01-15 12:09:26 -0300

    classes de dominio iniciadas

--- src/test/java/com/roberto/helpdesk/domain/Cliente.java
+++ src/test/java/com/roberto/helpdesk/domain/Cliente.java
@@ -7,4 +7,24 @@ public class Cliente extends Pessoa {
 	
 	private List<Chamado> chamados = new ArrayList<>();
 
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+	
+	
+	
+	
+
 }
