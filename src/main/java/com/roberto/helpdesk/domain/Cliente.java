7acd70dd52afd41438311e48b366fc36c895d60b
robertocajueiro <beto.cajueiro@gmail.com>
2023-01-16 17:53:32 -0300

    criacao do crud cliente

--- src/main/java/com/roberto/helpdesk/domain/Cliente.java
+++ src/main/java/com/roberto/helpdesk/domain/Cliente.java
@@ -2,12 +2,14 @@ package com.roberto.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.roberto.helpdesk.domain.enums.Perfil;
+import com.roberto.helpdesk.dtos.ClienteDTO;
 
 @Entity
 public class Cliente extends Pessoa {
@@ -25,6 +27,18 @@ public class Cliente extends Pessoa {
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 		addPerfil(Perfil.CLIENTE);
 	}
 
