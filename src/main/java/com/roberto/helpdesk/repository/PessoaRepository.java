faa778f27abebe8f7ce126d7857a6af2cf1b55a9
robertocajueiro <beto.cajueiro@gmail.com>
2023-01-15 22:58:13 -0300

    excecao para cpf e email ja cadastrado

--- src/main/java/com/roberto/helpdesk/repository/PessoaRepository.java
+++ src/main/java/com/roberto/helpdesk/repository/PessoaRepository.java
@@ -1,9 +1,15 @@
 package com.roberto.helpdesk.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.roberto.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+	
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
+	
 
 }
--- src/main/java/com/roberto/helpdesk/services/TecnicoService.java
+++ src/main/java/com/roberto/helpdesk/services/TecnicoService.java
@@ -6,17 +6,23 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.roberto.helpdesk.domain.Pessoa;
 import com.roberto.helpdesk.domain.Tecnico;
 import com.roberto.helpdesk.dtos.TecnicoDTO;
+import com.roberto.helpdesk.repository.PessoaRepository;
 import com.roberto.helpdesk.repository.TecnicoRepository;
+import com.roberto.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.roberto.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
-	
+
 	@Autowired
 	private TecnicoRepository repository;
-	
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id));
@@ -28,8 +34,21 @@ public class TecnicoService {
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCPFEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
+	}
+
+	private void validaPorCPFEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
 	}
 
 }
