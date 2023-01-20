ebaa391386a52346d3d69253e2c026c7ab7290de
robertocajueiro <beto.cajueiro@gmail.com>
2023-01-20 18:08:38 -0300

    find all para chamado

--- src/main/java/com/roberto/helpdesk/services/DBService.java
+++ src/main/java/com/roberto/helpdesk/services/DBService.java
@@ -35,10 +35,23 @@ public class DBService {
 		Cliente cli1 = new Cliente(null, "Lucas Cajueiro", "19348188011", "lucas@mail.com", "123" );
 		
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
+		Chamado c2 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 012", "Primeiro Chamado", tec1, cli1);
+		Chamado c3 = new Chamado(null, Prioridade.ALTA, Status.ANDAMENTO, "Chamado 02", "Segundo Chamado", tec1, cli1);
+		Chamado c4 = new Chamado(null, Prioridade.BAIXA, Status.ANDAMENTO, "Chamado 03", "Terceiro Chamado", tec1, cli1);
+		Chamado c5 = new Chamado(null, Prioridade.ALTA, Status.ANDAMENTO, "Chamado 04", "Quarto Chamado", tec1, cli1);
+		Chamado c6 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 05", "Quinto Chamado", tec1, cli1);
+		Chamado c7 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 06", "sexto Chamado", tec1, cli1);
 		
 		tecnicoRepository.saveAll(Arrays.asList(tec1));
 		clienteRepository.saveAll(Arrays.asList(cli1));
+		
 		chamadoRepository.saveAll(Arrays.asList(c1));
+		chamadoRepository.saveAll(Arrays.asList(c2));
+		chamadoRepository.saveAll(Arrays.asList(c3));
+		chamadoRepository.saveAll(Arrays.asList(c4));
+		chamadoRepository.saveAll(Arrays.asList(c5));
+		chamadoRepository.saveAll(Arrays.asList(c6));
+		chamadoRepository.saveAll(Arrays.asList(c7));
 	}
 
 }
